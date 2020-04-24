72c005d9649361bf144cb6a0b8997e12bc832955
Jorge <jorgbell@ucm.es>
2020-04-24 16:27:19 +0200

    Escena 2 completada

--- P2/Assets/Scripts/PlayerController.cs
+++ P2/Assets/Scripts/PlayerController.cs
@@ -6,17 +6,23 @@ public class PlayerController : MonoBehaviour
 {
     public float velocityScale;
     Rigidbody2D rb;
-    bool input;
+    float input;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        input = false;
+        input = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        input = Input.GetAxisRaw("Horizontal");
+    }
+    private void FixedUpdate()
+    {
+
+        rb.velocity = new Vector2(input * velocityScale, 0);
+        rb.velocity.Normalize();
     }
 }
