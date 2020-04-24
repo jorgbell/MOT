aa0c4eea7c5b696b3a08e1705bb0852dbc92bb5d
Jorge <jorgbell@ucm.es>
2020-04-24 18:33:21 +0200

    Escena 3_Ball completada

---
+++ P2/Assets/Scripts/Ball.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed;
+    Vector2 dir;
+    Rigidbody2D rb;
+    bool input;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        input = Input.GetButton("Submit");
+    }
+
+    private void FixedUpdate()
+    { 
+        if (rb.isKinematic == true)
+        {
+            if (input)
+            {
+                float random = Random.Range(-1.0f, 1.0f);
+                dir = new Vector2(random, 1f);     
+                dir.Normalize();
+                rb.velocity = dir *speed;
+                rb.isKinematic = false;
+                transform.SetParent(null);
+            }       
+        }
+
+    }
+
+}
--- P2/Assets/Scripts/PlayerController.cs
+++ P2/Assets/Scripts/PlayerController.cs
@@ -7,14 +7,12 @@ public class PlayerController : MonoBehaviour
     public float velocityScale;
     Rigidbody2D rb;
     float input;
-    // Start is called before the first frame update
+ 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        input = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
         input = Input.GetAxisRaw("Horizontal");
