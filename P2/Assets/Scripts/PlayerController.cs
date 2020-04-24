85f1098dcf85a138830f00bc2b43ea3a1a743d73
rubbueno <rubbueno@ucm.es>
2020-04-24 15:56:38 +0200

    iniciada escena1

---
+++ P2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocityScale;
+    RigidBody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
