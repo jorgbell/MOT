d8e8bd1e3c9fb2d627fa21c571b73aff556eb840
rubbueno <rubbueno@ucm.es>
2020-04-25 11:56:59 +0200

    Escena DeathZone

---
+++ P2/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        LostBall ball = collision.gameObject.GetComponent<LostBall>();
+        if (ball != null)
+            ball.OnLost();
+        else
+            Destroy(collision.gameObject);
+    }
+}
---
+++ P2/Assets/Scripts/LostBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LostBall : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public Transform respawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    public void OnLost()
+    {
+        transform.position = respawn.position;
+        rb.isKinematic = true;
+        transform.SetParent(respawn.parent);
+        rb.velocity = new Vector2(0, 0);
+    }
+}
