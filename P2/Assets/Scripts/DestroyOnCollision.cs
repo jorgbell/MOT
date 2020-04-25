fece78c064e19f16fb1da57ea3609a2d1213ece1
rubbueno <rubbueno@ucm.es>
2020-04-25 12:11:41 +0200

    Escena Bricks

---
+++ P2/Assets/Scripts/DestroyOnCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnCollision : MonoBehaviour
+{
+    public int golpesAntesDeMorir, puntosGanados;
+    int golpesRecibidos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        golpesRecibidos = 0;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        golpesRecibidos++;
+        if (golpesRecibidos == golpesAntesDeMorir)
+            Destroy(this.gameObject);
+    }
+}
