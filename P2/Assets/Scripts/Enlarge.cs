051fb3b6c4f6bc19a88a26b8544fcfc1905e65a3
Jorge <jorgbell@ucm.es>
2020-04-26 11:37:52 +0200

    power ups a medias

---
+++ P2/Assets/Scripts/Enlarge.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enlarge : MonoBehaviour
+{
+    Vector3 originalScale;
+    const float PORCENTAJE = 0.25f;
+
+    private void Awake()
+    {
+        originalScale = transform.localScale;
+    }
+    private void OnEnable()
+    {   
+        float scale = originalScale.x + (originalScale.x * PORCENTAJE);
+        Vector3 newScale = new Vector3(scale, originalScale.y, originalScale.z);
+        transform.localScale = newScale;
+        newScale.x = 1 / scale;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).localScale = newScale;
+        }
+
+    }
+    private void OnDisable()
+    {
+        transform.localScale = originalScale;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).localScale = originalScale;
+        }
+    }
+}
---
+++ P2/Assets/Scripts/Laser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public GameObject fire;
+    public Sprite vausLaserPower;
+    Sprite originalSprite;
+    SpriteRenderer spriteRenderer;
+    private void Awake()
+    {
+        spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+        originalSprite = spriteRenderer.sprite;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Instantiate<GameObject>(fire, transform.position, Quaternion.identity);
+        }        
+    }
+    private void OnEnable()
+    {
+        spriteRenderer.sprite = vausLaserPower;
+    }
+    private void OnDisable()
+    {
+        spriteRenderer.sprite = originalSprite;
+    }
+}
